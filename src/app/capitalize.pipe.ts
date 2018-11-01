7c2c3e9244000bb26e3e72c67bc9504bcc9fc645
SAJIN <sajin.kasim@xpo.com>
2018-11-01 17:33:20 +0530

    Custom Pipe working Fine
    Signed-off-by: SAJIN <sajin.kasim@xpo.com>

--- src/app/capitalize.pipe.ts
+++ src/app/capitalize.pipe.ts
@@ -5,9 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CapitalizePipe implements PipeTransform {
 
-  transform(value: any, args?: any,b?:any): any {
-    console.log("*** ",args,b)
-    return "Hello saran";
+  transform(value: any, args?: any,): any {
+    let splitString:any,capitalizedString:any,replaceString:any;
+    splitString=value.split(" ");
+    
+    for(let i=0;i<splitString.length;i++){
+      replaceString=splitString[i].substring(0,1).toUpperCase();
+      splitString[i]=splitString[i].replace(splitString[i].substring(0,1),replaceString);
+      
+    }
+    capitalizedString=splitString.join(" ");
+    return capitalizedString ;
   }
 
 }
